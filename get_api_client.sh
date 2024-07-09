6dfdd3325b1b6962827db0d776be4ead8e3edfe5
Matt Kinnersley <kinnersleymatt@gmail.com>
2024-07-09 15:35:35 +0200

    Add export api client script

---
+++ get_api_client.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+git pull
+cd ../vestia/packages/web
+npx tsup app/client/index.ts --dts --format esm --env.VITE_API_URL https://api.vestia.dev/
+mv -f dist/* ../../../sdk/packages/api
