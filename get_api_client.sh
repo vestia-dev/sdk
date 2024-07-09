1069c4b035d0577762496b4d8f7bc0a12f6aa034
Matt Kinnersley <kinnersleymatt@gmail.com>
2024-07-09 15:42:18 +0200

    Sync

--- get_api_client.sh
+++ get_api_client.sh
@@ -4,5 +4,6 @@ git pull
 cd ../vestia/packages/web
 npx tsup app/client/index.ts --dts --format esm --env.VITE_API_URL https://api.vestia.dev/
 mv -f dist/* ../../../sdk/packages/api
-npm run changeset
+cd ../../../sdk
+npx changeset
 git add .
